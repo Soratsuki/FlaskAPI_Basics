2b515a9974bd098fb716106b0b18d3e1cc374a6b
soratsuki <obriansan@outlook.com>
2021-07-17 19:20:38 -0500

    Configured sqlite3 to enforce foreign key constraints

--- server.py
+++ server.py
@@ -3,18 +3,35 @@ from sqlite3 import Connection as SQLiteConnection
 from datetime import datetime
 
 from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from flask_sqlalchemy import SQLAlchemy
 #app
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
 app.config["SQL_TRACK_MODIFICATIONS"] = 0
 
+#Configure sqlite3 to enforce foreign key constraints 
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
+
 #models
 class User(db.Model):
+    """
+    USer Table in class form that will be represented in the database..
+
+    Args:
+        db ([type]): [description]
+    """
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(50))
     address = db.Column(db.String(200))
     phone = db.Column(db.String(50))
-    posts = db.relationship("BlogPost")
+    posts = db.relationship("BlogPost") #Relationship to another table called BlogPost
