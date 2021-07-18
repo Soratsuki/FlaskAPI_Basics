86b2db6095417f35f08704f22465e79d7cc41e72
soratsuki <obriansan@outlook.com>
2021-07-17 19:11:42 -0500

    Created a class that represents a table in our database. The User class was create.d

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from sqlite3 import Connection as SQLiteConnection
 from datetime import datetime
 
+from sqlalchemy import event
 #app
 app = Flask(__name__)
 
@@ -9,3 +10,11 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
 app.config["SQL_TRACK_MODIFICATIONS"] = 0
 
 #models
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    address = db.Column(db.String(200))
+    phone = db.Column(db.String(50))
+    posts = db.relationship("BlogPost")
