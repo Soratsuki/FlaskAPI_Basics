9e96c093d00a2ce9d791b657b021106f4d6dfb32
soratsuki <obriansan@outlook.com>
2021-07-26 02:35:03 -0500

    Created the insert at the beginning and the insert at the end methods for the linked_list

--- linked_list.py
+++ linked_list.py
@@ -20,6 +20,34 @@ class Linked_List:
         ll_string += "None"
         return ll_string
 
+    def insert_beginning(self,data):
+        new_node = Node(data,self.head)
+        self.head = new_node
+
+    def insert_at_end(self,data):
+        """
+        First checks if linked List is empty, and if it is then inserts at the beginning.
+
+        Then checks if we know the value of last_node, if not we traverse the linked_list 
+        until we reach the tail. 
+        At the tail we set the next_node as the new node created, and assign that value as the new last_node.
+
+        Args:
+            data ([type]): [description]
+        """
+        if self.head is None:
+            self.insert_beginning(data)
+
+        if self.last_node is None:
+            node = self.head
+            while node.next_node:
+                #to reach the end of the linked list
+                node = node.next_node
+
+        node.next_node = Node(data,None)
+        self.last_node = node.next_node
+
+
 
 ll = Linked_List()
 
@@ -29,5 +57,8 @@ node2 = Node("data2", node3)
 node1 = Node("data1", node2)
 
 ll.head = node1
+ll.insert_beginning(50)
+ll.insert_beginning(150)
+ll.insert_at_end(400)
 print(ll)
 
